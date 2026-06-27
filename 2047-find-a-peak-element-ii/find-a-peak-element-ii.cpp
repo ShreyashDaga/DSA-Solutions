2b6eb8aee90c94928a32a714238af9cdf1d1a14b
Shreyash <dagashreyash8@gmail.com>
2026-06-27 14:40:15 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (59.34%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,29 +1,74 @@
 class Solution {
 public:
+
+    int findMaxRow(vector<vector<int>>& mat, int col) {
+
+        int maxRow = 0;
+
+        for (int i = 1; i < mat.size(); i++) {
+
+            if (mat[i][col] > mat[maxRow][col]) {
+                maxRow = i;
+            }
+        }
+
+        return maxRow;
+    }
+
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
 
         int m = mat.size();
         int n = mat[0].size();
 
-        for (int i = 0; i < m; i++) {
+        int low = 0;
+        int high = n - 1;
 
-            for (int j = 0; j < n; j++) {
+        while (low <= high) {
 
-                int up = (i == 0) ? -1 : mat[i - 1][j];
-                int down = (i == m - 1) ? -1 : mat[i + 1][j];
-                int left = (j == 0) ? -1 : mat[i][j - 1];
-                int right = (j == n - 1) ? -1 : mat[i][j + 1];
+            int mid = low + (high - low) / 2;
 
-                if (mat[i][j] > up &&
-                    mat[i][j] > down &&
-                    mat[i][j] > left &&
-                    mat[i][j] > right) {
+            // Find the row having the maximum element in the middle column
+            int maxRow = findMaxRow(mat, mid);
 
-                    return {i, j};
-                }
+            int left = -1;
+            int right = -1;
+
+            // Left neighbour
+            if (mid > 0) {
+                left = mat[maxRow][mid - 1];
+            }
+
+            // Right neighbour
+            if (mid < n - 1) {
+                right = mat[maxRow][mid + 1];
+            }
+
+            // Peak found
+            if (mat[maxRow][mid] > left &&
+                mat[maxRow][mid] > right) {
+
+                vector<int> ans;
+                ans.push_back(maxRow);
+                ans.push_back(mid);
+
+                return ans;
+            }
+
+            // Search left half
+            else if (left > mat[maxRow][mid]) {
+                high = mid - 1;
+            }
+
+            // Search right half
+            else {
+                low = mid + 1;
             }
         }
 
-        return {-1, -1};
+        vector<int> ans;
+        ans.push_back(-1);
+        ans.push_back(-1);
+
+        return ans;
     }
 };
